2419e6a8bbb3b69e7ecb925e035b4c127d098387
tharathip.kul@gmail.com <kuang.acad@gmail.com>
2025-02-17 01:32:00 +0700

    update

--- src/app.py
+++ src/app.py
@@ -7,12 +7,11 @@ import os
 # models
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_openai.embeddings import OpenAIEmbeddings
-from langgraph.checkpoint.memory import MemorySaver
-
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv(override=True)
 
-@st.cache_resource
 def get_thread():
     thread_id = str(uuid4())
     return thread_id
@@ -20,19 +19,17 @@ def get_thread():
 # layout
 st.set_page_config(page_title="Talk with Lecture", page_icon="assets/logo.png")
 
-
-thread_id = get_thread()
 # get the graph
 llm = ChatOpenAI(model="gpt-4o", streaming=True)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store_path = "src/assets/vectorstores"
-checkpointer = MemorySaver()
+os.makedirs("src/assets/checkpoints", exist_ok=True)
+conn = sqlite3.connect("src/assets/checkpoints/checkpoints.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn)
 graph = get_graph(llm, embeddings, vector_store_path, checkpointer)
 
 # display graph
 with st.sidebar:
-    st.subheader("Thread ID")
-    st.markdown(f"{thread_id}")
     st.subheader("ReAct Agent")
     st.markdown("This is a ReAct agent that uses OpenAI's GPT-4o model to answer questions about dental topics.")
     graph_image = graph.get_graph().draw_mermaid_png()
@@ -45,28 +42,31 @@ with st.sidebar:
         st.markdown(f"- {file}")
 
 # state initialization
+intro_message = "Hi! I am a Talkable Dental Lecture. How can I help?"
 if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Chatbot
-if len(st.session_state.messages) == 0:
-    intro_message = "Hi! I am a Talkable Dental Lecture. How can I help?"
-    st.session_state.messages.append({"role": "assistant", "content": intro_message})
+    st.session_state.messages = [{"role": "assistant", "content": intro_message}]
 
 # Display and render old chat messages
 for message in st.session_state.messages:
-    st.chat_message(message['role']).write(message['content'])
+    with st.chat_message(message['role']):
+        st.write(message['content'])
 
 if prompt := st.chat_input("How can I help?"):
-    st.session_state.thread_id = thread_id
+    if len(st.session_state.messages) <= 1:
+        st.session_state.thread_id = get_thread()
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # invoke the graph
-    config = {"configurable": {"thread_id": thread_id}}
-    response = graph.invoke({"messages": [("human", prompt)]}, config)
+    
+    with st.chat_message('user'):
+        st.markdown(prompt)
+
+    with st.spinner("Thinking..."):
+        # invoke the graph
+        config = {"configurable": {"thread_id": st.session_state.thread_id}}
+        response = graph.invoke({"messages": [("human", prompt)]}, config)
 
     # update the session state
-    st.session_state.messages.append({"role": "user", "content": prompt})
     st.session_state.messages.append({"role": "assistant", "content": response['messages'][-1].content})
 
     # write the response
-    st.chat_message('assistant').write(response['messages'][-1].content)
+    with st.chat_message('assistant'):
+        st.markdown(response['messages'][-1].content)
